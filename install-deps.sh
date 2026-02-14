81267be0a764708d8391f49663ea62ddc5e9f617
Apollo Daniel <developer.apollo.mail@gmail.com>
2026-02-14 18:10:40 -0300

    feat: add install dependencies script

---
+++ install-deps.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Install all dependencies used by the i3wm config files.
+# Requires: pacman + yay (AUR helper)
+# Distro: CachyOS / Arch Linux
+
+set -e
+
+# --- Pacman packages (official repos) ---
+PACMAN_PKGS=(
+    # Window manager & bar
+    i3-wm
+    i3status
+    i3lock
+
+    # X11 utilities
+    xorg-xrandr
+    xorg-xset
+    xss-lock
+    dex
+
+    # Launcher & notifications
+    rofi
+    dunst
+
+    # Terminal & file manager
+    kitty
+    nemo
+
+    # Screenshot & clipboard
+    maim
+    slop
+    xclip
+
+    # Audio control (pactl)
+    libpulse
+
+    # Networking & keyring
+    network-manager-applet
+    gnome-keyring
+
+    # Rofi window switcher dep
+    wmctrl
+
+    # Icon theme (used by rofi)
+    papirus-icon-theme
+
+    # Misc
+    xdg-utils
+)
+
+# --- AUR packages (via yay) ---
+AUR_PKGS=(
+    rofi-greenclip       # clipboard manager for rofi
+    rofi-bluetooth-git   # bluetooth menu for rofi
+    boomer-git           # zoom tool
+    nightshift           # blue light filter
+)
+
+echo "=== Installing pacman packages ==="
+sudo pacman -S --needed --noconfirm "${PACMAN_PKGS[@]}"
+
+echo ""
+echo "=== Installing AUR packages (yay) ==="
+yay -S --needed --noconfirm "${AUR_PKGS[@]}"
+
+echo ""
+echo "=== All dependencies installed ==="
